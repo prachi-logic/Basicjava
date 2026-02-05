c676770ab3ef7c26851661c2a98854193897d028
prachi agrawal <agrawalprachi860@gmail.com>
2026-02-05 19:46:48 +0530

    added daysofweek java program

---
+++ daysofweek.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class daysofweek {
+    public static void main(String[] args) {
+        int day;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the day no of a week:");
+        day=sc.nextInt();
+
+        switch(day)
+        {
+            case 1: System.out.println("Monday");
+            break;
+            case 2: System.out.println("Tuesday");
+            break;
+            case 3: System.out.println("Wednesday");
+            break;
+            case 4: System.out.println("Thrusday");
+            break;
+            case 5: System.out.println("Friday");
+            break;
+            case 6: System.out.println("Saturday");
+            break;
+            case 7: System.out.println("Sunday");
+            break;
+            default:
+                System.out.println("invalid option inputted:");
+
+
+    }
+}
+    
+}
