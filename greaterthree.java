551ed2ff5251d40ee8404f20c4d8a799cacbd058
prachi agrawal <agrawalprachi860@gmail.com>
2026-02-08 22:21:41 +0530

    added greaterthree java program

---
+++ greaterthree.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class greaterthree{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter first number:");
+        a=sc.nextInt();
+        System.out.println("Enter second number:");
+        b=sc.nextInt();
+        System.out.println("Enter third variable:");
+        c=sc.nextInt();
+        if (a>b&&a>c){
+            System.out.println("Largest num is :" +a);}
+        else if(b>a&& b>c){
+            System.out.println("Largest num is:"+b);}
+        else{
+            System.out.println("Largest num is :"+c);}
+        }
+    }
